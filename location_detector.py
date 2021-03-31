4bf077306ea83e88ba84ba0bfb64b4625335d7b3
Deniz1998 <81053502+Deniz1998@users.noreply.github.com>
2021-04-01 00:33:45 +0100

    Update location_detector.py

--- location_detector.py
+++ location_detector.py
@@ -1,5 +1,19 @@
+def info_conv(place):
+    information = {
+        'Rankine': 'https://universitystory.gla.ac.uk/building/?id=18 ',
+        'Library': 'https://universitystory.gla.ac.uk/building/?id=69 ',
+        'James Watt School of Engineering': 'https://universitystory.gla.ac.uk/building/?id=112',
+        'GUU': 'https://universitystory.gla.ac.uk/building/?id=50',
+        'Frasers': 'https://universitystory.gla.ac.uk/building/?id=71',
+        'Boyd Orr': 'https://universitystory.gla.ac.uk/building/?id=40',
+        'QMU': 'https://universitystory.gla.ac.uk/building/?id=19',
+        'EAST': 'https://en.wikipedia.org/wiki/University_of_Glasgow',
+        'WEST': 'https://en.wikipedia.org/wiki/University_of_Glasgow',
+    }
+    return information.get(place, 'None')
+
+
 def location_conv(name):
-    words = name.split(' ')
     address = {
         '00:2a:10:57:35:31': 'Rankine ',
         '00:2a:10:9b:8a:b1': 'Library ',
@@ -11,14 +25,35 @@ def location_conv(name):
         '70:79:b3:2d:6f:a1': 'EAST',
         'b8:62:1f:ac:60:81': 'WEST',
     }
-    output = ' '
-    for word in words:
-        output += address.get(word, 'User Travelling') + ' '
-    return output
+    return address.get(name, 'User Travelling')
 
 
-name = input('>')
-location_conv(name)
-print(location_conv(name))
+start = input('enter start location>')
+start_location = location_conv(start)
+loc_int = []
+loc_int.append(start_location)
+loc = []
+loc.append(start_location)
 
 
+for i in range(1,10,1):
+    name = input('>')
+    loc_int.append(location_conv(name))
+    if loc_int[i] != loc_int[i - 1]:
+        loc.append(loc_int[i])
+        if loc[-1] == 'User Travelling':
+            z = 1
+        else:
+            print('Do you like this place?')
+            x = input()
+            if x == 'Yes':
+                info_conv(loc[-1])
+                print(info_conv(loc[-1]))
+            else:
+                print('Too Sad (❁´◡`❁)')
+
+    else:
+        print("same location")
+
+
+print(loc)
